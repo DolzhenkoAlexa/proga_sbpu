aedb587f9fe55a592db7d8df78752c907aa34f2d
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-11-28 01:01:00 +0300

    Styleguide in quotient.c

--- quotient.c
+++ quotient.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int findQ(int a, int b) {
+int findQ(int a, int b)
+{
     if (b == 0) {
         printf("Нельзя делить на ноль!\n");
         return 0;
     }
+    
     int q = 0;
     while ((q + 1) * abs(b) <= abs(a)) {
         q++;
     }
+    
     // Определяем знак
     if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
         return -q;
@@ -17,8 +20,10 @@ int findQ(int a, int b) {
     return q;
 }
 
-int main() {
+int main()
+{
     int a, b;
+    
     printf("Введите два целых числа a и b: \n");
     scanf("%d %d", &a, &b);
     
