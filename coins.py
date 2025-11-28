dbcda112dd82c5428495cb637064203e13946f55
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-11-28 14:25:24 +0300

    New version of coins.py

--- coins.py
+++ coins.py
@@ -1,15 +1,22 @@
-num = int(input())
+sum = int(input("Введите сумму,которую нужно разменять: "))
+coins = []
 
-for c10 in range(num // 10, -1, -1):
-    rem1 = num - c10 * 10
-    for c9 in range(rem1 // 9, -1, -1):
-        rem2 = rem1 - c9 * 9
-        for c8 in range(rem2 // 8, -1, -1):
-            rem3 = rem2 - c8 * 8
-            if rem3 == 0:
-                print('Колво 10: ', c10)
-                print('Колво 9: ', c9)
-                print('Колво 8: ', c8)
-                exit()
+for i in range(3):
+    coin = int(input(f"Введите номинал монеты {i+1}: ")) # Введена абстрактность
+    coins.append(coin)
+
+coins.sort(reverse=True) # Cортируем по убыванию
+coin_val1, coin_val2, coin_val3 = coins[0], coins[1], coins[2]
+
+for amount1 in range(sum // coin_val1, -1, -1):
+    remaining  = sum - amount1 * coin_val1
+    for amount2 in range(remaining  // coin_val2, -1, -1):
+        remaining  = remaining  - amount2 * coin_val2
+        if coin_val3 != 0 and remaining % coin_val3 == 0: # Третий цикл убран
+            amount3 = remaining  // coin_val3
+            print("Количество монет номинала", coin_val1, ":", amount1)
+            print("Количество монет номинала", coin_val2, ":", amount2)
+            print("Количество монет номинала", coin_val3, ":", amount3)
+            exit()
 
 print("-42!")
