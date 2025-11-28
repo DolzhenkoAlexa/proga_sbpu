0f28967df191883def01d9acd674b9465402f8af
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-11-28 16:00:53 +0300

    Update coins.py

--- coins.py
+++ coins.py
@@ -9,11 +9,11 @@ coins.sort(reverse=True) # Cортируем по убыванию
 coin_val1, coin_val2, coin_val3 = coins[0], coins[1], coins[2]
 
 for amount1 in range(sum // coin_val1, -1, -1):
-    remaining  = sum - amount1 * coin_val1
-    for amount2 in range(remaining  // coin_val2, -1, -1):
-        remaining  = remaining  - amount2 * coin_val2
-        if coin_val3 != 0 and remaining % coin_val3 == 0: # Третий цикл убран
-            amount3 = remaining  // coin_val3
+    remaining1  = sum - amount1 * coin_val1
+    for amount2 in range(remaining1  // coin_val2, -1, -1):
+        remaining2  = remaining1  - amount2 * coin_val2
+        if coin_val3 != 0 and remaining2 % coin_val3 == 0: # Третий цикл убран
+            amount3 = remaining2  // coin_val3
             print("Количество монет номинала", coin_val1, ":", amount1)
             print("Количество монет номинала", coin_val2, ":", amount2)
             print("Количество монет номинала", coin_val3, ":", amount3)
